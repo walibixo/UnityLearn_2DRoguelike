19777fc7304edace2b5a08f2d0b9e772482b131a
walibixo <chatot.antoine@gmail.com>
2024-12-05 11:30:37 +0100

    Add animation with coroutine on player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector2Int _cellPosition;
+    [SerializeField] private float _moveDuration;
+
+    private Vector2Int _currentCellPosition;
+    private bool _isMoving;
+
+    private Coroutine _moveCoroutine;
 
     private BoardManager _boardManager;
 
@@ -20,6 +26,11 @@ public class PlayerController : MonoBehaviour
                 GameManager.Instance.StartNewGame();
             }
 
+            return;
+        }
+
+        if (_isMoving)
+        {
             return;
         }
 
@@ -43,12 +54,12 @@ public class PlayerController : MonoBehaviour
 
     public void Spawn(Vector2Int position)
     {
-        SetPosition(position);
+        SetPosition(position, true);
     }
 
     private void TryMove(Vector2Int direction)
     {
-        Vector2Int newPosition = _cellPosition + direction;
+        Vector2Int newPosition = _currentCellPosition + direction;
 
         if (!_boardManager.IsPassable(newPosition))
         {
@@ -65,13 +76,46 @@ public class PlayerController : MonoBehaviour
         else if (cellObject.PlayerTryEnter())
         {
             SetPosition(newPosition);
-            cellObject.PlayerEntered();
         }
     }
 
-    private void SetPosition(Vector2Int position)
+    private void SetPosition(Vector2Int position, bool immediate = false)
     {
-        _cellPosition = position;
+        if (_moveCoroutine != null)
+        {
+            StopCoroutine(_moveCoroutine);
+        }
+
+        _moveCoroutine = StartCoroutine(Move(position, immediate));
+    }
+
+    private IEnumerator Move(Vector2Int position, bool immediate = false)
+    {
+        _isMoving = true;
+
+        if (!immediate && _moveDuration > 0)
+        {
+            Vector3 startingPos = transform.position;
+            Vector3 finalPos = _boardManager.CellToWorld(position);
+
+            float elapsedTime = 0;
+            while (elapsedTime < _moveDuration)
+            {
+                transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / _moveDuration));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+
         transform.position = _boardManager.CellToWorld(position);
+        _currentCellPosition = position;
+
+        _isMoving = false;
+
+        var cellObject = _boardManager.GetObject(position);
+        if (cellObject != null)
+        {
+            cellObject.PlayerEntered();
+        }
     }
 }
