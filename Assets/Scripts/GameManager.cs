d432b167147e7c184df0cdf3fb3b96c4796bb1e7
walibixo <chatot.antoine@gmail.com>
2025-11-06 08:14:07 +0100

    Add survivor logs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,6 +22,31 @@ public class GameManager : MonoBehaviour
 
     private int _levelCount;
     private int _foodAmount;
+
+    private readonly string[] _survivalLogs = new[]
+    {
+        "Only the hope of others keeps me moving N.E..",
+        "Every dawn means one more step towards the North-East.",
+        "They say the sun still shines N.E. I have to see it.",
+        "The map is clear. The survivors are North-East.",
+        "I won't stop until I see their fires in the N.E. distance.",
+        "This journey ends in the North-East, one way or another.",
+        "The road N.E. is a graveyard. I pray it's not mine.",
+        "Just one more day of walking. Just one more, heading N.E..",
+        "I can almost smell the smoke from the N.E. encampment.",
+        "My legs ache, but my hope is set on the North-East.",
+        "Another day, another horde, still pushing for the N.E..",
+        "The voices of the N.E. survivors call me forward.",
+        "Finding them is the only reason to keep fighting this far N.E..",
+        "Maybe they have medicine N.E. Maybe they have answers.",
+        "This entire trek N.E. is based on a whisper. A deadly whisper.",
+        "I follow a dead rumor N.E. It's all I have left.",
+        "If I stop, the horde wins. N.E. is the only escape.",
+        "I'm one cut away from becoming one of them. Press N.E..",
+        "They said 'go N.E.' like it was easy. It's a lie, but I obey.",
+        "Don't look back. Just keep moving N.E..",
+        "I'm tired of walking. I must be close to the N.E. now."
+    };
 
     void Awake()
     {
@@ -104,7 +131,12 @@ public class GameManager : MonoBehaviour
     public void UpdateFoodAmount(int relativeAmount = 0)
     {
         _foodAmount += relativeAmount;
-        _foodLabel.text = "Food : " + _foodAmount.ToString().Replace('9', 'P').Replace('8', 'B');
+        _foodLabel.text = $"Survival Log: Day {FixMissingFontNumbers(_levelCount)}{Environment.NewLine}Rations Left: {FixMissingFontNumbers(_foodAmount)}{Environment.NewLine}{_survivalLogs[_levelCount % _survivalLogs.Length]}";
+    }
+
+    private string FixMissingFontNumbers(object input)
+    {
+        return input.ToString().Replace('9', 'P').Replace('8', 'B');
     }
 
     private void SaveGameState()
